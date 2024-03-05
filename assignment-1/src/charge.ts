4295095cbf1028c58c57b221451d77d3b9e2c63c
妹尾弦 <gengineer.web@gmail.com>
2024-03-05 11:58:07 +0900

    charge.tsのリファクタリング

--- assignment-1/src/charge.ts
+++ assignment-1/src/charge.ts
@@ -9,42 +9,53 @@ export type Receipt = {
 };
 
 export type Payment = {
-  type: string;
+  type: 'CASH' | 'COUPON'; // 支払い方法はCASH(現金)かCOUPON(クーポン)のいずれか
   percentage?: number;
   amount?: number;
 };
 
 export function charge(invoice: Invoice, payments: Payment[]) {
-  const total = invoice.total;
+  const { total } = invoice;
+  // 預かり金。支払いが行われるたびに加算される
   let deposit = 0;
 
-  payments
-    .sort((payment) => (payment.type !== 'CASH' ? -1 : 1))
-    .map((payment) => {
-      if (payment.type === 'COUPON') {
-        if (payment.percentage != null) {
-          deposit += Math.floor(total * (payment.percentage / 100));
-        } else {
-          deposit += payment.amount || 0;
-        }
-      } else {
+  // paymentsをtype=CASHのものを先に処理するようにソート(paymentsの破壊的変更を防ぐ)
+  const sortedPayments = payments.slice().sort((payment) => (payment.type !== 'CASH' ? -1 : 1));
+
+  // 配列生成するmap関数である必要がないのでforEachに変更
+  sortedPayments.forEach((payment) => {
+    const { type, amount, percentage } = payment;
+
+    switch (type) {
+      case 'CASH': {
+        // 過払いの場合は'OverCharge'としてエラーを起こす
         if (deposit >= total) {
           throw new Error('OverCharge');
         }
-        deposit += payment.amount || 0;
+        deposit += amount ?? 0;
+        break;
       }
-    });
+      case 'COUPON': {
+        // nullとの比較になっていたが、型でnullは許容されていないのでundefinedとの比較に変更
+        if (percentage !== undefined) {
+          deposit += Math.floor(total * (percentage / 100));
+        } else {
+          deposit += amount ?? 0;
+        }
+        break;
+      }
+    }
+  });
+
+  // depositがtotalより多い場合は'Shortage'としてエラーを起こす
   if (total > deposit) {
     throw new Error('Shortage');
   }
 
-  let isCoupon = true;
-  for (let i = 0; i < payments.length; i++) {
-    if (payments[i].type !== 'COUPON') {
-      isCoupon = false;
-      continue;
-    }
-  }
-  if (isCoupon) return { total, deposit, change: 0 };
-  return { total: total, deposit: deposit, change: deposit - total };
+  // クーポンの使用有無を判定する
+  const isCoupon = payments.every((payment) => payment.type === 'COUPON');
+  // お釣りの計算
+  const change = isCoupon ? 0 : deposit - total;
+
+  return { total, deposit, change };
 }
